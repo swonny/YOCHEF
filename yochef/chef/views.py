4c8657a9933752f8350550f424fedeefae39d5f6
Zeus6768 <akaps6768@gmail.com>
2021-07-02 11:51:00 +0900

    Zeus - registerChef html view Modified

--- yochef/chef/views.py
+++ yochef/chef/views.py
@@ -3,36 +3,49 @@ from .models import *
 
 # Create your views here.
 
-def registerChef(request, page_num, chef=None, post=None):
-    if page_num == '0':
-        return render(request, 'registerChef_1.html')
-    # elif page_num == '1':
-    #     chef = Chef()
-    #     # profileImage = File()
-    #     # profileImage = request.GET['profileImg']  # profileImage
-    #     chef.nickname = request.GET['nickname']
-    #     chef.spec = request.GET['spec']
-    #     chef.snsLink = request.GET['snsLink']
-    #     chef.blogLink = request.GET['blogLink']
-    #     chef.youtubeLink = request.GET['youtubeLink']
-    #     chef.save()
-    #     return render(request, 'registerChef_2.html', {'chef': chef})
-    # elif page_num == '2': 
-    #     post = Post()
-    #     post.category = request.GET['category']
-    #     post.region = request.GET['region']
-    #     post.regionDetail = request.GET['regionDetail']
-    #     post.save()
-    #     return render(request, 'registerChef_3.html', {'post': post})
-    # elif page_num == '3':
-    #     post.title = request.GET['title']
-    #     # postCoverImage = File()
-    #     # postCoverImage.attachment = request.GET['attachment']
-    #     post.introduce = request.GET['introduce']
-    #     post.spec = request.GET['spec']
-    elif page_num == '4':
-        return render(request, '/')
-    return render(request, f'registerChef_{int(page_num)+1}.html')
+def registerChef(request, page_num, customer_id):
+    if request.method == "POST":
+        if page_num == '0':
+            return render(request, 'registerChef_1.html')
+        elif page_num == '1':
+            customer = request.user.Customer.objects.get(id=customer_id)     # Customer() 가져오는 방법?
+            chef = Chef(customer=customer)
+            # profileImage = File()                             # File() 가져오는 방법?
+            # profileImage = request.POST['profileImg']
+            chef.nickname = request.POST['nickname']
+            chef.spec = request.POST['spec']
+            chef.snsLink = request.POST['snsLink']
+            chef.blogLink = request.POST['blogLink']
+            chef.youtubeLink = request.POST['youtubeLink']
+            chef.save()
+            return render(request, 'registerChef_2.html')
+        elif page_num == '2': 
+            customer = request.user.Customer.objects.get(id=customer_id)
+            chef = Chef.objects.get(customer=customer)
+            post = Post(chef=chef)
+            post.category = request.POST['category']
+            post.region = request.POST['region']
+            post.regionDetail = request.POST['regionDetail']
+            post.save()
+            return render(request, 'registerChef_3.html')
+        elif page_num == '3':
+            customer = request.user.Customer.objects.get(id=customer_id)
+            chef = Chef.objects.get(customer=customer)
+            post = Post.objects.get(chef=chef)
+            post.title = request.POST['title']
+            # postCoverImage = File()
+            # postCoverImage.attachment = request.POST['attachment']
+            post.introduce = request.POST['introduce']
+            post.spec = request.POST['spec']
+            post.save()
+            return render(request, 'registerChef_4.html')
+        elif page_num == '4':
+            customer = request.user.Customer.objects.get(id=customer_id)
+            chef = Chef.objects.get(customer=customer)
+            post = Post.objects.get(chef=chef)
+            chef.movingPrice = request.Post['movingPrice']
+            chef.save()
+            return render(request, '/')
 
 def chefSchedule(request):
     return render(request, 'chefSchedule.html')
